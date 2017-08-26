b9fda965f2123c816cbb61c22ae75adcf70876ce
DESKTOP-HPAKHL7\alphe <e0031928@u.nus.edu>
2017-08-26 11:06:06 +0800

    added testing

--- addressbook-level1-master/src/seedu/addressbook/AddressBook.java
+++ addressbook-level1-master/src/seedu/addressbook/AddressBook.java
@@ -56,6 +56,7 @@ public class AddressBook {
      * and may be subjected to review by UI experts or technical writers. Note
      * that Some of the strings below include '%1$s' etc to mark the locations
      * at which java String.format(...) method can insert values.
+     * testing
      * =========================================================================
      */
     private enum PersonProperty  {NAME, EMAIL, PHONE};
